6cdf5c385852fb71511cc06f65a1f828b23cf7c8
Bianca Niemann <payrollsollutions@gmail.com>
2023-05-12 13:24:41 +0200

    14 tests passed commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -9,7 +9,24 @@ function App() {
   const [fullTotal, setFullTotal] = useState('')
   const [isTotalled, setIsTotalled] = useState(false)
   const[start, setStart] = useState(false)
+  const [isDecimal, setIsDecimal] = useState(false)
+  const [isZero, setIsZero] = useState(false)
+  const [isOperator, setIsOperator] = useState(false)
 
+  useEffect(()=> {
+    if(digit.includes('.')){
+      setIsDecimal(true)
+    } else {
+      setIsDecimal(false)
+    }
+
+    if(digit.startsWith(0)){
+      setIsZero(true)
+    } else {
+      setIsZero(false)
+    }
+    
+  },[digit])
 
   function insertOperator(e) {
     if (isUpdated === false) {
@@ -22,21 +39,31 @@ function App() {
   }
 
   function insertDigit(e) {
-    setIsUpdated(false)
-    if (operator.length > 0) {
-      setTotal(prevTotal => prevTotal + operator)
-      setOperator('')
-      setDigit('')
-      setDigit(prevDigit => prevDigit + e)
-    } else if (operator.length <= 0) {
-      console.log(digit.at(0))
-      setStart(true)
-      
-      setDigit(prevDigit => prevDigit + e)
-      setFullTotal('')
-      setIsTotalled(false)
-    }
-  }
+    let selectedDigit = e
+
+    if(e === '.' && isDecimal === false){
+        selectedDigit = '.'
+      } else if(e === '.' && isDecimal === true){
+        selectedDigit = ''
+      }else if(e === 0 && isZero === false){
+        selectedDigit = 0
+      } else if(e === 0 && isZero === true){
+        selectedDigit = ''
+      }
+
+      setIsUpdated(false)
+      if (operator.length > 0) {
+        setTotal(prevTotal => prevTotal + operator)
+        setOperator('')
+        setDigit('')
+       setDigit(prevDigit => prevDigit + selectedDigit)
+      } else if (operator.length <= 0) {
+        setStart(true)
+        setDigit(prevDigit => prevDigit + selectedDigit)
+        setFullTotal('')
+        setIsTotalled(false)
+      }
+  }  
 
   function addSum(e) {
     setIsTotalled(true)
@@ -58,7 +85,6 @@ function App() {
     setIsTotalled(false)
   }
 
-
   return (
     <>
       <button id="equals" onClick={() => addSum(digit)}>=</button>
@@ -78,9 +104,9 @@ function App() {
       <button id="divide" onClick={() => insertOperator('/')}>/</button>
       <button id="decimal" onClick={() => insertDigit('.')}>.</button>
       <button id="clear" onClick={()=> clearScreen()}>AC</button>
-      {!isTotalled && !start && <input id="display" value='0' onChange={e => setDigit(e.target.value)} />}
-      {!isTotalled && start && <input id="display" value={digit} onChange={e => setDigit(e.target.value)} />}
-      {isTotalled && <input id="display" value={fullTotal} onChange={e => setDigit(e.target.value)} />}
+      {!isTotalled && !start && <input id="display" value='0' onChange={e => checkZero(e)} />}
+      {!isTotalled && start && <input id="display" value={digit} onChange={e => checkZero(e)} />}
+      {isTotalled && <input id="display" value={fullTotal} onChange={e => checkZero(e)} />}
       <div>{total}</div>
     </>
   )
