2ee6595108f8f6a832277561791bae42ad327bef
Bianca Niemann <payrollsollutions@gmail.com>
2023-05-15 11:20:27 +0200

    16/16 tests passed for js code

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,38 @@ function App() {
   const [isUpdated, setIsUpdated] = useState(false)
   const [fullTotal, setFullTotal] = useState('')
   const [isTotalled, setIsTotalled] = useState(false)
-  const[start, setStart] = useState(false)
+  const [start, setStart] = useState(false)
   const [isDecimal, setIsDecimal] = useState(false)
   const [isZero, setIsZero] = useState(false)
-  const [isOperator, setIsOperator] = useState(false)
 
-  useEffect(()=> {
-    if(digit.includes('.')){
+  useEffect(() => {
+    if (digit.includes('.')) {
       setIsDecimal(true)
     } else {
       setIsDecimal(false)
     }
 
-    if(digit.startsWith(0)){
+    if (digit.startsWith(0)) {
       setIsZero(true)
     } else {
       setIsZero(false)
     }
+
     
-  },[digit])
+    if(isTotalled && isUpdated){
+      setTotal(fullTotal)
+      setIsTotalled(false)
+      setStart(true)
+    }
+//3 + 5 * 6 - 2 / 4 should produce 32.5 or 11.5 
+  }, [digit, isTotalled, isUpdated])
 
   function insertOperator(e) {
     if (isUpdated === false) {
       setTotal(prevTotal => prevTotal + digit)
-      setOperator(e)
+      setOperator(prevOperator => prevOperator + e)
     } else {
-      setOperator(e)
+      setOperator(prevOperator => prevOperator + e)
     }
     setIsUpdated(true)
   }
@@ -41,41 +47,51 @@ function App() {
   function insertDigit(e) {
     let selectedDigit = e
 
-    if(e === '.' && isDecimal === false){
-        selectedDigit = '.'
-      } else if(e === '.' && isDecimal === true){
-        selectedDigit = ''
-      }else if(e === 0 && isZero === false){
-        selectedDigit = 0
-      } else if(e === 0 && isZero === true){
-        selectedDigit = ''
-      }
+    if (e === '.' && isDecimal === false) {
+      selectedDigit = '.'
+    } else if (e === '.' && isDecimal === true) {
+      selectedDigit = ''
+    } else if (e === 0 && isZero === false) {
+      selectedDigit = 0
+    } else if (e === 0 && isZero === true) {
+      selectedDigit = ''
+    }
+   
+    setIsUpdated(false)
 
-      setIsUpdated(false)
-      if (operator.length > 0) {
-        setTotal(prevTotal => prevTotal + operator)
-        setOperator('')
-        setDigit('')
-       setDigit(prevDigit => prevDigit + selectedDigit)
-      } else if (operator.length <= 0) {
-        setStart(true)
-        setDigit(prevDigit => prevDigit + selectedDigit)
-        setFullTotal('')
-        setIsTotalled(false)
+    if (operator.length === 1) {
+      setTotal(prevTotal => prevTotal + operator)
+      setOperator('')
+      setDigit('')
+      setDigit(prevDigit => prevDigit + selectedDigit)
+    } else if (operator.length > 1) {
+      let newOperators
+      if (operator.endsWith('-')) {
+        newOperators = operator.slice(-2)
+      } else {
+        newOperators = operator.slice(-1)
       }
-  }  
+      setTotal(prevTotal => prevTotal + newOperators)
+    } else if (operator.length <= 0) {
+      setStart(true)
+      setDigit(prevDigit => prevDigit + selectedDigit)
+      setFullTotal('')
+      setIsTotalled(false)
+    }
+    
+  }
 
   function addSum(e) {
     setIsTotalled(true)
     setTotal(prevTotal => prevTotal + e)
     setFullTotal(eval(total + e))
-    setTotal('')
+   
     setDigit('')
     setOperator('')
     setStart(false)
   }
 
-  function clearScreen(){
+  function clearScreen() {
     setTotal('')
     setDigit('')
     setOperator('')
@@ -103,7 +119,7 @@ function App() {
       <button id="multiply" onClick={() => insertOperator('*')}>x</button>
       <button id="divide" onClick={() => insertOperator('/')}>/</button>
       <button id="decimal" onClick={() => insertDigit('.')}>.</button>
-      <button id="clear" onClick={()=> clearScreen()}>AC</button>
+      <button id="clear" onClick={() => clearScreen()}>AC</button>
       {!isTotalled && !start && <input id="display" value='0' onChange={e => checkZero(e)} />}
       {!isTotalled && start && <input id="display" value={digit} onChange={e => checkZero(e)} />}
       {isTotalled && <input id="display" value={fullTotal} onChange={e => checkZero(e)} />}
